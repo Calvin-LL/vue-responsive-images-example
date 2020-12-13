9c80a01ed09d1ead3da490eff7b3478864be60e1
Calvin Liang <me@calvin.sh>
2020-12-13 15:33:51 -0800

    feat: add png example

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,11 @@
         require('@/assets/Macaca_nigra_self-portrait_large.jpg?srcset&quality=90')
       "
     />
+    <img
+      style="width: 50vw"
+      src="@/assets/Simple_Periodic_Table_Chart-en.png"
+      :srcset="require('@/assets/Simple_Periodic_Table_Chart-en.png?srcset')"
+    />
     <!-- for modern browsers: -->
     <picture>
       <source
@@ -30,6 +35,22 @@
           backgroundColor: require('@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder'),
         }"
         src="@/assets/Macaca_nigra_self-portrait_large.jpg"
+      />
+    </picture>
+    <picture>
+      <source
+        type="image/webp"
+        :srcset="
+          require('@/assets/Simple_Periodic_Table_Chart-en.png?srcset&format=webp&quality=90')
+        "
+      />
+      <source
+        type="image/png"
+        :srcset="require('@/assets/Simple_Periodic_Table_Chart-en.png?srcset')"
+      />
+      <img
+        style="width: 50vw"
+        src="@/assets/Simple_Periodic_Table_Chart-en.png"
       />
     </picture>
   </div>
