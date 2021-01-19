5ef33d74d8f571235e48205a0b2efebb683e44e0
Calvin Liang <me@calvin.sh>
2021-01-18 23:11:04 -0800

    chore(deps): update packages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,45 @@ module.exports = {
   publicPath: ".",
   chainWebpack: (config) => {
     config.module
+      .rule("images-srcset")
+      .before("images")
+      .test(/\.(png|jpe?g|webp|tiff?)$/i)
+      // if the import url looks like "some.png?srcset..."
+      .oneOf("srcset")
+      .resourceQuery(/srcset/)
+      .use("srcset")
+      .loader("webpack-image-srcset-loader")
+      .options({
+        sizes: ["480w", "1024w", "1920w", "2560w", "original"],
+        esModule: false,
+      });
+
+    config.module
+      .rule("images-resize")
+      .after("images")
+      .test(/\.(png|jpe?g|webp|tiff?)$/i)
+      // if the import url looks like "some.png?srcset..."
+      .oneOf("srcset")
+      .resourceQuery(/srcset/)
+      .use("resize")
+      .loader("webpack-image-resize-loader")
+      .options({
+        fileLoader: "url-loader",
+        fileLoaderOptionsGenerator: (options, existingOptions) => ({
+          ...existingOptions,
+          fallback: {
+            ...existingOptions.fallback,
+            options: {
+              ...defaultFileLoaderOptionsGenerator(
+                options,
+                existingOptions.fallback.options
+              ),
+            },
+          },
+        }),
+      });
+
+    config.module
       .rule("images")
       .test(/\.(png|jpe?g|gif|webp|tiff?)$/i)
       // if the import url looks like "some.png?placeholder..."
@@ -12,25 +51,6 @@ module.exports = {
       .options({
         format: "hex",
         esModule: false,
-      })
-      .end()
-      .end()
-      // if the import url looks like "some.png?srcset..."
-      .oneOf("srcset")
-      .resourceQuery(/srcset/)
-      .use("srcset")
-      .loader("webpack-image-srcset-loader")
-      .options({
-        sizes: ["480w", "1024w", "1920w", "2560w", "original"],
-        esModule: false,
-      })
-      .end()
-      .use("resize")
-      .loader("webpack-image-resize-loader")
-      .options({
-        fileLoaderOptions: {
-          outputPath: "img",
-        },
       })
       .end()
       .end()
